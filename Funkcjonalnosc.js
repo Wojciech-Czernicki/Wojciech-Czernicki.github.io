9be29b3a843a6770f10860a054e7e64b1c192ceb
Wojciech-Czernicki <wojciech_cz@wp.pl>
2023-11-16 16:16:14 +0100

    SEE

---
+++ Funkcjonalnosc.js
@@ -0,0 +1,119 @@
+// Funkcjonalnosc.js
+var obrazy;
+var wprowadzonaOdpowiedz = '';
+var obraz = document.getElementById('obraz');
+var obecnyObrazIndex = 0;
+var poprawnaOdpowiedzElement = document.getElementById('poprawnaOdpowiedz');
+
+
+async function pobierzBazeDanych() {
+    try {
+        const response = await fetch('Baza.json');
+        if (!response.ok) {
+            throw new Error(`Nieudane pobieranie danych. Kod odpowiedzi: ${response.status}`);
+        }
+        const data = await response.json();
+
+        if (Array.isArray(data.obrazy)) {
+            // Ustawienie wartoúci domyúlnych dla brakujπcych pÛl
+            obrazy = data.obrazy.map(obraz => ({
+                id: obraz.id || 0,
+                tytul: obraz.tytul || "Brak tytu≥u",
+                lokalizacja: obraz.lokalizacja || "Brak lokalizacji",
+                odpowiedz: obraz.odpowiedz || "Brak odpowiedzi"
+            }));
+
+            zaladujLosowyObraz();
+        } else {
+            throw new Error('Niepoprawny format danych w pliku JSON.');
+        }
+    } catch (error) {
+        console.error('B≥πd pobierania danych:', error);
+    }
+}
+
+
+
+function zaladujLosowyObraz() {
+    const minId = 1;
+    const maxId = 4;
+    obecnyObrazIndex = Math.floor(Math.random() * (maxId - minId + 1)) + minId;
+    zaladujObraz();
+}
+
+
+function zaladujObraz() {
+    const aktualnyObraz = obrazy[obecnyObrazIndex];
+    obraz.src = aktualnyObraz.lokalizacja;
+    poprawnaOdpowiedzElement.textContent = `Poprawna Odpowiedü: ${aktualnyObraz.odpowiedz}`;
+}
+function dodajLitera(litera, event) {
+    const enterKeyCode = 13;
+    if (litera === 'Enter' || (event && event.keyCode === enterKeyCode)) {
+        sprawdzOdpowiedz();
+        return;
+    }
+
+    if (wprowadzonaOdpowiedz.length < 25) {
+        wprowadzonaOdpowiedz += litera;
+        aktualizujWprowadzonaOdpowiedz();
+    }
+}
+
+
+function usunLitera() {
+    wprowadzonaOdpowiedz = wprowadzonaOdpowiedz.slice(0, -1);
+    aktualizujWprowadzonaOdpowiedz();
+}
+
+function aktualizujWprowadzonaOdpowiedz() {
+    document.getElementById('wprowadzonaOdpowiedz').textContent = wprowadzonaOdpowiedz;
+}
+
+function sprawdzOdpowiedz() {
+    const odpowiedz = document.getElementById("odpowiedz").value.toLowerCase();
+    const poprawnaOdpowiedz = obrazy[obecnyObrazIndex].odpowiedz.toLowerCase();
+
+    if (odpowiedz === poprawnaOdpowiedz) {
+        document.getElementById("wynik").textContent = "Odpowiedü poprawna!";
+    } else {
+        document.getElementById("wynik").textContent = "Odpowiedü niepoprawna. SprÛbuj ponownie.";
+    }
+
+    // Przejdü do nastÍpnego obrazu
+    obecnyObrazIndex++;
+    if (obecnyObrazIndex < obrazy.length) {
+        zaladujObraz();
+        document.getElementById("odpowiedz").value = "";
+        document.getElementById("wprowadzonaOdpowiedz").textContent = ""; // WyczyúÊ wyúwietlonπ odpowiedü
+    } else {
+        document.getElementById("wynik").textContent = "Gra zakoÒczona!";
+    }
+}
+
+// Obs≥uga klawiatury
+document.addEventListener('keydown', function (event) {
+    if (event.key.length === 1) {
+        dodajLitera(event.key.toUpperCase());
+    } else if (event.key === 'Backspace') {
+        usunLitera();
+    } else if (event.key === 'Enter') {
+        dodajLitera('Enter', event);
+    }
+});
+
+// Rozpocznij grÍ po za≥adowaniu strony
+document.addEventListener('DOMContentLoaded', function () {
+    pobierzBazeDanych();
+});
+
+function rozpocznijGre() {
+    // Ukryj ekran poczπtkowy
+    document.getElementById('startScreen').style.display = 'none';
+
+    // Pokaø ekran gry
+    document.getElementById('graScreen').style.display = 'flex';
+
+    // Rozpocznij grÍ
+    pobierzBazeDanych();
+}
---
+++ skrypt.js
@@ -0,0 +1,4 @@
+document.getElementById('startButton').addEventListener('click', function () {
+    // Po klikniêciu przycisku "Start", przekieruj u¿ytkownika do strony gry.
+    window.location.href = 'Glowny.html';
+});
