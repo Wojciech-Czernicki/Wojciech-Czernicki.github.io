a40c46a4a4d3ec7ba964427edb3523d491b5fdd8
Wojciech-Czernicki <wojciech_cz@wp.pl>
2023-11-16 14:03:25 +0100

    Summary

--- skrypt.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('startButton').addEventListener('click', function () {
-    // Po klikniêciu przycisku "Start", przekieruj u¿ytkownika do strony gry.
-    window.location.href = 'Glowny.html';
-});
