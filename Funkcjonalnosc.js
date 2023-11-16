135d6d6032f8eccbcd1152e135dc064a3dab7078
Wojciech_Czernicki <115473470+Wojciech-Czernicki@users.noreply.github.com>
2023-11-16 10:46:13 +0100

    Update Funkcjonalnosc.js

--- Funkcjonalnosc.js
+++ Funkcjonalnosc.js
@@ -4,7 +4,7 @@ var wprowadzonaOdpowiedz = '';
 var obraz = document.getElementById('obraz');
 var obecnyObrazIndex = 0;
 var poprawnaOdpowiedzElement = document.getElementById('poprawnaOdpowiedz');
-const wprowadzoneLiteryContainer = document.getElementById('wprowadzoneLitery');
+
 
 async function pobierzBazeDanych() {
     try {
@@ -30,10 +30,6 @@ async function pobierzBazeDanych() {
     } catch (error) {
         console.error('B��d pobierania danych:', error);
     }
-    const response = await fetch('Baza_zdjec.json');
-    const data = await response.json();
-    obrazy = data.obrazy;
-    zaladujLosowyObraz();
 }
 
 
@@ -42,7 +38,6 @@ function zaladujLosowyObraz() {
     const minId = 1;
     const maxId = 4;
     obecnyObrazIndex = Math.floor(Math.random() * (maxId - minId + 1)) + minId;
-    obecnyObrazIndex = Math.floor(Math.random() * obrazy.length);
     zaladujObraz();
 }
 
@@ -51,89 +46,64 @@ function zaladujObraz() {
     const aktualnyObraz = obrazy[obecnyObrazIndex];
     obraz.src = aktualnyObraz.lokalizacja;
     poprawnaOdpowiedzElement.textContent = `Poprawna Odpowied�: ${aktualnyObraz.odpowiedz}`;
-    poprawnaOdpowiedzElement.textContent = `Poprawna Odpowiedź: ${aktualnyObraz.odpowiedz}`;
 }
 function dodajLitera(litera, event) {
     const enterKeyCode = 13;
     if (litera === 'Enter' || (event && event.keyCode === enterKeyCode)) {
-
-function dodajLitera(litera) {
-    const enterKeyCode = 13; // Kod klawisza Enter
-    if (litera === 'Enter' || event.keyCode === enterKeyCode) {
         sprawdzOdpowiedz();
         return;
     }
+
     if (wprowadzonaOdpowiedz.length < 25) {
         wprowadzonaOdpowiedz += litera;
         aktualizujWprowadzonaOdpowiedz();
-        dodajDoWprowadzonychLiter(litera);
     }
 }
 
-function dodajDoWprowadzonychLiter(litera) {
-    const literaElement = document.createElement('div');
-    literaElement.classList.add('wprowadzona-litera');
-    literaElement.textContent = litera;
-    wprowadzoneLiteryContainer.appendChild(literaElement);
-}
 
 function usunLitera() {
     wprowadzonaOdpowiedz = wprowadzonaOdpowiedz.slice(0, -1);
     aktualizujWprowadzonaOdpowiedz();
-    usunZwprowadzonychLiter();
-}
-
-function usunZwprowadzonychLiter() {
-    const ostatniaLitera = wprowadzoneLiteryContainer.lastElementChild;
-    if (ostatniaLitera) {
-        wprowadzoneLiteryContainer.removeChild(ostatniaLitera);
-    }
 }
 
 function aktualizujWprowadzonaOdpowiedz() {
+    document.getElementById('wprowadzonaOdpowiedz').textContent = wprowadzonaOdpowiedz;
+}
+
+function sprawdzOdpowiedz() {
+    const odpowiedz = document.getElementById("odpowiedz").value.toLowerCase();
     const poprawnaOdpowiedz = obrazy[obecnyObrazIndex].odpowiedz.toLowerCase();
 
     if (odpowiedz === poprawnaOdpowiedz) {
         document.getElementById("wynik").textContent = "Odpowied� poprawna!";
-        document.getElementById("wynik").textContent = "Odpowiedź poprawna!";
     } else {
         document.getElementById("wynik").textContent = "Odpowied� niepoprawna. Spr�buj ponownie.";
-        document.getElementById("wynik").textContent = "Odpowiedź niepoprawna. Spróbuj ponownie.";
     }
 
     // Przejd� do nast�pnego obrazu
-    // Przejdź do następnego obrazu
     obecnyObrazIndex++;
     if (obecnyObrazIndex < obrazy.length) {
         zaladujObraz();
         document.getElementById("odpowiedz").value = "";
         document.getElementById("wprowadzonaOdpowiedz").textContent = ""; // Wyczy�� wy�wietlon� odpowied�
-        document.getElementById("wprowadzonaOdpowiedz").textContent = "";
-        usunWprowadzoneLitery();
     } else {
         document.getElementById("wynik").textContent = "Gra zako�czona!";
-        document.getElementById("wynik").textContent = "Gra zakończona!";
     }
 }
 
 // Obs�uga klawiatury
 document.addEventListener('keydown', function (event) {
-function usunWprowadzoneLitery() {
-    wprowadzoneLiteryContainer.innerHTML = '';
-}
-
-// Obsługa klawiatury
-document.addEventListener('keydown', function(event) {
     if (event.key.length === 1) {
         dodajLitera(event.key.toUpperCase());
     } else if (event.key === 'Backspace') {
+        usunLitera();
+    } else if (event.key === 'Enter') {
+        dodajLitera('Enter', event);
     }
 });
 
 // Rozpocznij gr� po za�adowaniu strony
 document.addEventListener('DOMContentLoaded', function () {
-// Rozpocznij grę po załadowaniu strony
-document.addEventListener('DOMContentLoaded', function() {
     pobierzBazeDanych();
 });
 
