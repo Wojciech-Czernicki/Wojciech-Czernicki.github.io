b07a05877139ea8f83312289e6a7df2eabd326a1
Wojciech-Czernicki <wojciech_cz@wp.pl>
2023-11-16 10:34:04 +0100

    Update Funkcjonalnosc.js

--- Funkcjonalnosc.js
+++ Funkcjonalnosc.js
@@ -4,52 +4,29 @@ var wprowadzonaOdpowiedz = '';
 var obraz = document.getElementById('obraz');
 var obecnyObrazIndex = 0;
 var poprawnaOdpowiedzElement = document.getElementById('poprawnaOdpowiedz');
-
+const wprowadzoneLiteryContainer = document.getElementById('wprowadzoneLitery');
 
 async function pobierzBazeDanych() {
-    try {
-        const response = await fetch('Baza.json');
-        if (!response.ok) {
-            throw new Error(`Nieudane pobieranie danych. Kod odpowiedzi: ${response.status}`);
-        }
-        const data = await response.json();
-
-        if (Array.isArray(data.obrazy)) {
-            // Ustawienie wartoœci domyœlnych dla brakuj¹cych pól
-            obrazy = data.obrazy.map(obraz => ({
-                id: obraz.id || 0,
-                tytul: obraz.tytul || "Brak tytu³u",
-                lokalizacja: obraz.lokalizacja || "Brak lokalizacji",
-                odpowiedz: obraz.odpowiedz || "Brak odpowiedzi"
-            }));
-
-            zaladujLosowyObraz();
-        } else {
-            throw new Error('Niepoprawny format danych w pliku JSON.');
-        }
-    } catch (error) {
-        console.error('B³¹d pobierania danych:', error);
-    }
+    const response = await fetch('Baza_zdjec.json');
+    const data = await response.json();
+    obrazy = data.obrazy;
+    zaladujLosowyObraz();
 }
-
-
 
 function zaladujLosowyObraz() {
-    const minId = 2;
-    const maxId = 4;
-    obecnyObrazIndex = Math.floor(Math.random() * (maxId - minId + 1)) + minId;
+    obecnyObrazIndex = Math.floor(Math.random() * obrazy.length);
     zaladujObraz();
 }
-
 
 function zaladujObraz() {
     const aktualnyObraz = obrazy[obecnyObrazIndex];
     obraz.src = aktualnyObraz.lokalizacja;
-    poprawnaOdpowiedzElement.textContent = `Poprawna OdpowiedŸ: ${aktualnyObraz.odpowiedz}`;
+    poprawnaOdpowiedzElement.textContent = `Poprawna Odpowiedź: ${aktualnyObraz.odpowiedz}`;
 }
-function dodajLitera(litera, event) {
-    const enterKeyCode = 13;
-    if (litera === 'Enter' || (event && event.keyCode === enterKeyCode)) {
+
+function dodajLitera(litera) {
+    const enterKeyCode = 13; // Kod klawisza Enter
+    if (litera === 'Enter' || event.keyCode === enterKeyCode) {
         sprawdzOdpowiedz();
         return;
     }
@@ -57,13 +34,28 @@ function dodajLitera(litera, event) {
     if (wprowadzonaOdpowiedz.length < 25) {
         wprowadzonaOdpowiedz += litera;
         aktualizujWprowadzonaOdpowiedz();
+        dodajDoWprowadzonychLiter(litera);
     }
 }
 
+function dodajDoWprowadzonychLiter(litera) {
+    const literaElement = document.createElement('div');
+    literaElement.classList.add('wprowadzona-litera');
+    literaElement.textContent = litera;
+    wprowadzoneLiteryContainer.appendChild(literaElement);
+}
 
 function usunLitera() {
     wprowadzonaOdpowiedz = wprowadzonaOdpowiedz.slice(0, -1);
     aktualizujWprowadzonaOdpowiedz();
+    usunZwprowadzonychLiter();
+}
+
+function usunZwprowadzonychLiter() {
+    const ostatniaLitera = wprowadzoneLiteryContainer.lastElementChild;
+    if (ostatniaLitera) {
+        wprowadzoneLiteryContainer.removeChild(ostatniaLitera);
+    }
 }
 
 function aktualizujWprowadzonaOdpowiedz() {
@@ -75,24 +67,29 @@ function sprawdzOdpowiedz() {
     const poprawnaOdpowiedz = obrazy[obecnyObrazIndex].odpowiedz.toLowerCase();
 
     if (odpowiedz === poprawnaOdpowiedz) {
-        document.getElementById("wynik").textContent = "OdpowiedŸ poprawna!";
+        document.getElementById("wynik").textContent = "Odpowiedź poprawna!";
     } else {
-        document.getElementById("wynik").textContent = "OdpowiedŸ niepoprawna. Spróbuj ponownie.";
+        document.getElementById("wynik").textContent = "Odpowiedź niepoprawna. Spróbuj ponownie.";
     }
 
-    // PrzejdŸ do nastêpnego obrazu
+    // Przejdź do następnego obrazu
     obecnyObrazIndex++;
     if (obecnyObrazIndex < obrazy.length) {
         zaladujObraz();
         document.getElementById("odpowiedz").value = "";
-        document.getElementById("wprowadzonaOdpowiedz").textContent = ""; // Wyczyœæ wyœwietlon¹ odpowiedŸ
+        document.getElementById("wprowadzonaOdpowiedz").textContent = "";
+        usunWprowadzoneLitery();
     } else {
-        document.getElementById("wynik").textContent = "Gra zakoñczona!";
+        document.getElementById("wynik").textContent = "Gra zakończona!";
     }
 }
 
-// Obs³uga klawiatury
-document.addEventListener('keydown', function (event) {
+function usunWprowadzoneLitery() {
+    wprowadzoneLiteryContainer.innerHTML = '';
+}
+
+// Obsługa klawiatury
+document.addEventListener('keydown', function(event) {
     if (event.key.length === 1) {
         dodajLitera(event.key.toUpperCase());
     } else if (event.key === 'Backspace') {
@@ -102,18 +99,8 @@ document.addEventListener('keydown', function (event) {
     }
 });
 
-// Rozpocznij grê po za³adowaniu strony
-document.addEventListener('DOMContentLoaded', function () {
+// Rozpocznij grę po załadowaniu strony
+document.addEventListener('DOMContentLoaded', function() {
     pobierzBazeDanych();
 });
 
-function rozpocznijGre() {
-    // Ukryj ekran pocz¹tkowy
-    document.getElementById('startScreen').style.display = 'none';
-
-    // Poka¿ ekran gry
-    document.getElementById('graScreen').style.display = 'flex';
-
-    // Rozpocznij grê
-    pobierzBazeDanych();
-}
